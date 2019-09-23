03daa2e4c5730fde99eddf90e4028b648d62fdff
Chris Kalafarski <chris@farski.com>
2019-09-23 12:19:24 -0400

    Add mpck layer

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ aws cloudformation deploy \
         --stack-name fixer-state-machine-prototype \
         --capabilities CAPABILITY_IAM \
         --parameter-overrides \
-                AwsXraySdkLambdaLayerVersionArn=arn:aws:lambda:us-east-2:127213743756:layer:aws-xray-sdk:1 \
-                FfmepgLambdaLayerVersionArn=arn:aws:lambda:us-east-2:127213743756:layer:ffmpeg:1
+                AwsXraySdkLambdaLayerVersionArn=arn:aws:lambda:us-east-1:561178107736:layer:aws-xray:1 \
+                FfmepgLambdaLayerVersionArn=arn:aws:lambda:us-east-1:561178107736:layer:ffmepg-farski-test:1 \
+                MpckLambdaLayerVersionArn=arn:aws:lambda:us-east-1:561178107736:layer:mpck-farski-test:1
