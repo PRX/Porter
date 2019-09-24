17d53b3736fb12bec50596082c380cd8cd28cb30
Chris Kalafarski <chris@farski.com>
2019-09-24 13:18:07 -0400

    Update ECR push script

--- ecr-push.sh
+++ ecr-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-$(aws ecr get-login --no-include-email --region us-east-2 --profile prx_it_services)
-docker build -t sf-test .
-docker tag sf-test:latest 127213743756.dkr.ecr.us-east-2.amazonaws.com/sf-test:latest
-docker push 127213743756.dkr.ecr.us-east-2.amazonaws.com/sf-test:latest
+export AWS_PROFILE=prx_legacy
+export AWS_DEFAULT_REGION=us-east-1
+
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker build -t rexif-prototype .
+docker tag rexif-prototype:latest 561178107736.dkr.ecr.us-east-1.amazonaws.com/rexif-prototype:latest
+docker push 561178107736.dkr.ecr.us-east-1.amazonaws.com/rexif-prototype:latest
