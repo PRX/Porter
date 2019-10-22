f37d0e5585bcf1a572551022b96367a4a0d4cb8f
Chris Kalafarski <chris@farski.com>
2019-10-22 14:11:30 -0400

    Clean up comments and logging in SNS relay Lambda function

--- lambdas/JobExecutionSnsTopicLambdaFunction/index.js
+++ lambdas/JobExecutionSnsTopicLambdaFunction/index.js
@@ -1,3 +1,7 @@
+// This function relays messages from an SNS topic to a Step Function state
+// machine. It passes the SNS message directly to the state machine as
+// the execution input.
+
 const AWSXRay = require('aws-xray-sdk');
 
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
@@ -5,6 +9,11 @@ const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const stepfunctions = new AWS.StepFunctions({ apiVersion: '2016-11-23' });
 
 exports.handler = async (event) => {
+  console.log(JSON.stringify({
+    msg: 'Starting execution',
+    job: event.Records[0].Sns.Message
+  }));
+
   await stepfunctions.startExecution({
     stateMachineArn: process.env.STATE_MACHINE_ARN,
     input: event.Records[0].Sns.Message
