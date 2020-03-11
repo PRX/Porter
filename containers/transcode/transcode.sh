6e5e7aee12f8201fdef175abf0ff60b704b78a9e
Chris Kalafarski <chris@farski.com>
2020-03-11 08:31:09 -0400

    Use explicit region and endpoint on STS CLI call

--- containers/transcode/transcode.sh
+++ containers/transcode/transcode.sh
@@ -13,6 +13,7 @@
 # STATE_MACHINE_FFMPEG_OUTPUT_FILE_OPTIONS
 # STATE_MACHINE_JOB_ID
 # STATE_MACHINE_S3_DESTINATION_WRITER_ROLE
+# STATE_MACHINE_AWS_REGION
 
 # Get the artifact file from S3
 aws s3 cp s3://"$STATE_MACHINE_ARTIFACT_BUCKET_NAME"/"$STATE_MACHINE_ARTIFACT_OBJECT_KEY" artifact
@@ -24,7 +25,7 @@ aws s3 cp s3://"$STATE_MACHINE_ARTIFACT_BUCKET_NAME"/"$STATE_MACHINE_ARTIFACT_OB
 
 # Assume a role that will have access to the S3 destination bucket, and use
 # that roles credentials for the s3 cp call
-role=$(aws sts assume-role --role-arn "$STATE_MACHINE_S3_DESTINATION_WRITER_ROLE" --role-session-name porter_transcode_task)
+role=$(aws sts assume-role --region "$STATE_MACHINE_AWS_REGION" --endpoint-url "https://sts.$STATE_MACHINE_AWS_REGION.amazonaws.com" --role-arn "$STATE_MACHINE_S3_DESTINATION_WRITER_ROLE" --role-session-name porter_transcode_task)
 export AWS_ACCESS_KEY_ID=$(echo $role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $role | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $role | jq -r .Credentials.SessionToken)
