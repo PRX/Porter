702fd619df87cbcceccea6b34f8d3e968c998276
Chris Kalafarski <chris@farski.com>
2020-03-26 19:38:12 -0400

    Use dynamic account ID for ECR push

--- containers/transcode/ecr-push.sh
+++ containers/transcode/ecr-push.sh
@@ -6,5 +6,5 @@ account_id=`aws sts get-caller-identity --output text --query 'Account'`
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$account_id.dkr.ecr.us-east-1.amazonaws.com"
 docker build -t porter .
-docker tag porter:latest 561178107736.dkr.ecr.us-east-1.amazonaws.com/porter:latest
-docker push 561178107736.dkr.ecr.us-east-1.amazonaws.com/porter:latest
+docker tag porter:latest "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter:latest"
+docker push "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter:latest"
