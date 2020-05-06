7485b89bef1d182abb711eb52223aad821f31fe1
Chris Kalafarski <chris@farski.com>
2020-05-06 15:56:32 -0400

    Add tests for file detection function

---
+++ test/unit/lambdas/SourceTypeLambdaFunction.test.js
@@ -0,0 +1,61 @@
+const handler = require('../../../lambdas/SourceTypeLambdaFunction/index')
+  .handler;
+
+const AWS = require('aws-sdk-mock');
+
+test('detects type for S3 objects', async () => {
+  AWS.mock(
+    'S3',
+    'getObject',
+    Buffer.from(require('fs').readFileSync('test/samples/jpg.jpg')),
+  );
+
+  const result = await handler({
+    Artifact: {
+      BucketName: 'myBucket',
+      ObjectKey: 'foo.bar',
+    },
+  });
+
+  expect(result.Extension).toEqual('jpg');
+
+  AWS.restore('S3');
+});
+
+test('detects type for S3 objects with goofy extensions', async () => {
+  AWS.mock(
+    'S3',
+    'getObject',
+    Buffer.from(require('fs').readFileSync('test/samples/jpg.png')),
+  );
+
+  const result = await handler({
+    Artifact: {
+      BucketName: 'myBucket',
+      ObjectKey: 'foo.bar',
+    },
+  });
+
+  expect(result.Extension).toEqual('jpg');
+
+  AWS.restore('S3');
+});
+
+test('does not detects type for undetectable S3 objects', async () => {
+  AWS.mock(
+    'S3',
+    'getObject',
+    Buffer.from(require('fs').readFileSync('test/samples/empty')),
+  );
+
+  const result = await handler({
+    Artifact: {
+      BucketName: 'myBucket',
+      ObjectKey: 'foo.bar',
+    },
+  });
+
+  expect(result.Extension).toBeUndefined();
+
+  AWS.restore('S3');
+});
