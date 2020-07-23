d00414864e35b751fe1e0d9879b12bfd20538d22
Chris Kalafarski <chris@farski.com>
2020-07-23 11:40:08 -0400

    Begin work on integration tests

--- test/integration/execution.test.js
+++ test/integration/execution.test.js
@@ -1,12 +1,33 @@
-const fs = require('fs');
-const { yamlParse, yamlDump } = require('yaml-cfn');
+// require('dotenv').config();
+
+// const AWS = require('aws-sdk');
+// const stepfunctions = new AWS.StepFunctions({
+//   apiVersion: '2016-11-23',
+//   region: 'us-east-1',
+// });
 
 test('integration test', async () => {
-  const yaml = fs.readFileSync('template.yml', 'utf8');
-  const cfn = yamlParse(yaml);
-
-  const json = fs.readFileSync('state-machine.asl.json', 'utf8');
-  const asl = JSON.parse(json);
-
-  expect(1).toEqual(1);
+  //   expect.assertions(1);
+  //   const req = await stepfunctions
+  //     .startExecution({
+  //       stateMachineArn: process.env.PORTER_STATE_MACHINE_ARN,
+  //       input: JSON.stringify({
+  //         Job: {
+  //           Id: 'porter-test-no-op',
+  //           Source: {
+  //             Mode: 'HTTP',
+  //             URL: 'http://example.com/',
+  //           },
+  //         },
+  //       }),
+  //     })
+  //     .promise();
+  //   await new Promise((r) => setTimeout(r, 5000));
+  //   const desc = await stepfunctions
+  //     .describeExecution({
+  //       executionArn: req.executionArn,
+  //     })
+  //     .promise();
+  //   const output = JSON.parse(desc.output);
+  //   await expect(output.JobResult.Job.Id).resolves().toEqual('porter-test-no-op');
 });
