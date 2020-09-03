eababd9b28ade479a74bfbcdfac6ec20167826b5
Chris Kalafarski <chris@farski.com>
2020-09-03 12:26:44 -0400

    Update transcode Docker image tag

--- containers/transcode/ecr-push.sh
+++ containers/transcode/ecr-push.sh
@@ -5,6 +5,6 @@ export AWS_PROFILE=prx-legacy
 account_id=`aws sts get-caller-identity --output text --query 'Account'`
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$account_id.dkr.ecr.us-east-1.amazonaws.com"
-docker build -t porter .
-docker tag porter:latest "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter:latest"
-docker push "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter:latest"
+docker build -t porter-transcode .
+docker tag porter-transcode:latest "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter-transcode:latest"
+docker push "$account_id.dkr.ecr.us-east-1.amazonaws.com/porter-transcode:latest"
