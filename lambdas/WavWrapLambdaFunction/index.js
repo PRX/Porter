7011d29301de963a4e4d33d21b75109f1896e45d
Chris Kalafarski <chris@farski.com>
2020-10-05 17:49:41 -0400

    Use arrow syntax

--- lambdas/WavWrapLambdaFunction/index.js
+++ lambdas/WavWrapLambdaFunction/index.js
@@ -5,7 +5,7 @@ const wavefile = require('prx-wavefile');
 
 function camelize(str) {
   return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
     .replace(/\s+/g, '');
