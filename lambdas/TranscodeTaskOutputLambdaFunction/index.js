b6aa9ed945e608aafa37394f3a521a4c4b4ef58f
Chris Kalafarski <chris@farski.com>
2020-12-13 22:53:58 -0500

    Run ffprobe on transcode task output and expose some values

--- lambdas/TranscodeTaskOutputLambdaFunction/index.js
+++ lambdas/TranscodeTaskOutputLambdaFunction/index.js
@@ -2,8 +2,23 @@
 // callback, this function takes the entire task input and builds a better
 // result that gets passed to the callback task.
 
+const AWSXRay = require('aws-xray-sdk');
+
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
 exports.handler = async (event) => {
   console.log(JSON.stringify({ msg: 'State input', input: event }));
+
+  // Get ffprobe results
+  const file = await s3
+    .getObject({
+      Bucket: process.env.ARTIFACT_BUCKET_NAME,
+      Key: `${event.Execution.Id}/transcode/ffprobe-${event.TaskIteratorIndex}.json`,
+    })
+    .promise();
+  const ffprobe = JSON.parse(file.Body.toString());
 
   const now = new Date();
 
@@ -11,6 +26,8 @@ exports.handler = async (event) => {
     Task: event.Task.Type,
     BucketName: event.Task.Destination.BucketName,
     ObjectKey: event.Task.Destination.ObjectKey,
+    Duration: +ffprobe.format.duration * 1000,
+    Size: +ffprobe.format.size,
     Time: now.toISOString(),
     Timestamp: +now / 1000,
   };
