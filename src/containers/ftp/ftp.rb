cf0a744b36744719bfa377a84f7eb7a3b69dfb04
Chris Kalafarski <chris@farski.com>
2021-12-17 11:35:17 -0500

    Improve copy task output

--- src/containers/ftp/ftp.rb
+++ src/containers/ftp/ftp.rb
@@ -27,6 +27,8 @@ load './ftp_files.rb'
 load './s3_files.rb'
 load './recorder.rb'
 
+RESULT_KEY = "#{ENV['STATE_MACHINE_EXECUTION_ID']}/copy/ftp-result-#{ENV['STATE_MACHINE_TASK_INDEX']}.json"
+
 logger = Logger.new($stdout)
 start_time = Time.now
 
@@ -35,57 +37,71 @@ logger.debug(JSON.dump({
   start_time: start_time
 }))
 
-# Count the transfers in CloudWatch Metrics
-recorder =
-  Recorder.new(
-    Aws::CloudWatch::Client.new,
-    'PRX/Porter',
-    [{ name: 'StateMachineName', value: ENV['STATE_MACHINE_NAME'] }]
-  )
+begin
+  # Count the transfers in CloudWatch Metrics
+  recorder =
+    Recorder.new(
+      Aws::CloudWatch::Client.new,
+      'PRX/Porter',
+      [{ name: 'StateMachineName', value: ENV['STATE_MACHINE_NAME'] }]
+    )
 
-recorder.record('FtpTransfers', 'Count', 1.0)
+  recorder.record('FtpTransfers', 'Count', 1.0)
 
-bucket = ENV['STATE_MACHINE_ARTIFACT_BUCKET_NAME']
-key = ENV['STATE_MACHINE_ARTIFACT_OBJECT_KEY']
-logger.debug(JSON.dump({
-  msg: 'Copying artifact to container',
-  bucket_name: bucket,
-  object_key: key
-}))
-s3 = Aws::S3::Client.new
-s3_files = S3Files.new(s3, logger)
-file = s3_files.download_file(bucket, key)
-
-ip = ENV['PUBLIC_IP']
-task = JSON.parse(ENV['STATE_MACHINE_TASK_JSON'])
-uri = URI.parse(task['URL'])
-md5 = task['MD5'].nil? ? false : task['MD5']
-timeout = task['Timeout'].nil? ? 1800 : task['Timeout']
-
-ftp_files = FtpFiles.new(logger, recorder)
-used_mode = ftp_files.upload_file(uri, file, md5: md5, public_ip: ip, mode: task['Mode'], timeout: timeout, max_attempts: 6)
-
-if used_mode
-  result_key = "#{ENV['STATE_MACHINE_EXECUTION_ID']}/copy/ftp-result-#{ENV['STATE_MACHINE_TASK_INDEX']}.json"
+  bucket = ENV['STATE_MACHINE_ARTIFACT_BUCKET_NAME']
+  key = ENV['STATE_MACHINE_ARTIFACT_OBJECT_KEY']
   logger.debug(JSON.dump({
-    msg: 'Copying state machine results file',
+    msg: 'Copying artifact to container',
     bucket_name: bucket,
-    object_key: result_key
+    object_key: key
   }))
+  s3 = Aws::S3::Client.new
+  s3_files = S3Files.new(s3, logger)
+  file = s3_files.download_file(bucket, key)
+
+  ip = ENV['PUBLIC_IP']
+  task = JSON.parse(ENV['STATE_MACHINE_TASK_JSON'])
+  uri = URI.parse(task['URL'])
+  md5 = task['MD5'].nil? ? false : task['MD5']
+  timeout = task['Timeout'].nil? ? 1800 : task['Timeout']
+  max_attempts = task['MaxAttempts'].nil? ? 6 : task['MaxAttempts']
+
+  ftp_files = FtpFiles.new(logger, recorder)
+  used_mode = ftp_files.upload_file(uri, file, md5: md5, public_ip: ip, mode: task['Mode'], timeout: timeout, max_attempts: max_attempts)
+
+  # Count the transfers in CloudWatch Metrics
+  end_time = Time.now
+  duration = end_time - start_time
+
+  recorder.record('FtpTransferDuration', 'Seconds', duration)
+
+  if used_mode
+
+    logger.debug(JSON.dump({
+      msg: 'Copying state machine results file',
+      bucket_name: bucket,
+      object_key: result_key
+    }))
+    s3.put_object(
+      bucket: bucket,
+      key: RESULT_KEY,
+      body: JSON.dump({
+        # All properties listed here will be included in the task result for
+        # this task.
+        Mode: used_mode
+      })
+    )
+  end
+rescue StandardError => e
   s3.put_object(
     bucket: bucket,
-    key: result_key,
+    key: RESULT_KEY,
     body: JSON.dump({
-      Mode: used_mode
+      Error: e.class.name,
+      ErrorMessage: e.message
     })
   )
 end
-
-# Count the transfers in CloudWatch Metrics
-end_time = Time.now
-duration = end_time - start_time
-
-recorder.record('FtpTransferDuration', 'Seconds', duration)
 
 logger.debug(JSON.dump({
   msg: 'ftp.rb end',
--- src/lambdas/ftp-copy-task-output/index.js
+++ src/lambdas/ftp-copy-task-output/index.js
@@ -1,12 +1,33 @@
+/* eslint-disable max-classes-per-file */
 // Because the result of a Fargate task is not sufficient for sending a proper
 // callback, this function takes the entire task input and builds a better
-// result that gets passed to the callback task.
+// result that gets passed to the callback task. The Fargate tasks should
+// always write a JSON file with information about the execution to S3. If it
+// includes an Error property, the FTP execution was unsuccessful, and this
+// Lambda should throw an error, to proxy the failure up to the state machine.
+// If the FTP operation was successful, there won't be an Error property, but
+// could be other properties like Mode, etc. All such properties will be
+// included in the task result.
 
 const AWSXRay = require('aws-xray-sdk');
 
 const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+class MissingFtpResultsError extends Error {
+  constructor(...params) {
+    super(...params);
+    this.name = 'MissingFtpResultsError';
+  }
+}
+
+class FtpOperationError extends Error {
+  constructor(...params) {
+    super(...params);
+    this.name = 'FtpOperationError';
+  }
+}
 
 exports.handler = async (event) => {
   console.log(JSON.stringify({ msg: 'State input', input: event }));
@@ -19,17 +40,26 @@ exports.handler = async (event) => {
     .promise();
   const ftpResult = JSON.parse(file.Body.toString());
 
-  const now = new Date();
+  if (!ftpResult) {
+    throw new MissingFtpResultsError('No Fargate results file found');
+  } else if (ftpResult.Error) {
+    // TODO Possible to throw the actual error class?
+    throw new FtpOperationError(
+      `${ftpResult.Error}: ${ftpResult.ErrorMessage}`,
+    );
+  } else {
+    const now = new Date();
 
-  const result = {
-    Task: event.Task.Type,
-    URL: event.Task.URL,
-    Mode: ftpResult.Mode,
-    Time: now.toISOString(),
-    Timestamp: +now / 1000,
-  };
+    const result = {
+      Task: event.Task.Type,
+      URL: event.Task.URL,
+      ...ftpResult,
+      Time: now.toISOString(),
+      Timestamp: +now / 1000,
+    };
 
-  console.log(JSON.stringify({ msg: 'Result', result }));
+    console.log(JSON.stringify({ msg: 'Result', result }));
 
-  return result;
+    return result;
+  }
 };
