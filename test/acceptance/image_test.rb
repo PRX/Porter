f9e8e08220b8edec2753dc4713617eb5b823db6c
Chris Kalafarski <chris@farski.com>
2021-12-17 15:02:35 -0500

    Add image task acceptance test

--- test/acceptance/image_test.rb
+++ test/acceptance/image_test.rb
@@ -4,29 +4,37 @@ require 'test_helper'
 require 'json'
 
 describe :porter do
-  describe :inspect do
-    it 'returns execution output for an inspect task' do
+  describe :image do
+    it 'returns execution output for an image task' do
       job = {
         Job: {
-          Id: 'porter-test-inspect',
+          Id: 'porter-test-image-resize',
           Source: {
             Mode: 'Data/URI',
             URI: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
           },
           Tasks: [
             {
-              Type: 'Inspect'
+              Type: 'Image',
+              Resize: {
+                Height: 50,
+                Width: 50
+              },
+              Destination: {
+                Mode: 'AWS/S3',
+                BucketName: 'prx-porter-sandbox',
+                ObjectKey: 'dev/sample/image/image.jpg'
+              }
             }
           ]
         }
       }
 
       job_test(job) do |output|
-        _(output['JobResult']['Job']['Id']).must_equal 'porter-test-inspect'
+        _(output['JobResult']['Job']['Id']).must_equal 'porter-test-image-resize'
         _(output['JobResult']['State']).must_equal 'DONE'
         _(output['JobResult']['FailedTasks']).must_equal []
         _(output['JobResult']['TaskResults'].length).must_equal 1
-        _(output['JobResult']['TaskResults'][0]['Inspection']['Image']['Format']).must_equal 'gif'
       end
     end
   end
