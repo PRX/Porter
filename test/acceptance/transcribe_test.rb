c96e994d9de07b791b4de07da3bd2dfafa32865a
Chris Kalafarski <chris@farski.com>
2021-12-17 10:53:39 -0500

    Add transcribe task tests

--- test/acceptance/transcribe_test.rb
+++ test/acceptance/transcribe_test.rb
@@ -5,6 +5,38 @@ require 'json'
 
 describe :porter do
   describe :transcribe do
+    it 'returns execution output for an transcribe task' do
+      job = {
+        Job: {
+          Id: 'porter-test-transcribe-output',
+          Source: {
+            Mode: 'HTTP',
+            URL: 'https://dovetail.prxu.org/152/245d0fe2-4171-4ebf-bea3-69deff3e9336/short.mp3'
+          },
+          Tasks: [
+            {
+              Type: 'Transcribe',
+              LanguageCode: 'en-US',
+              SubtitleFormats: ['vtt'],
+              Destination: {
+                Mode: 'AWS/S3',
+                BucketName: CONFIG.PORTER_TEST_BUCKET_NAME,
+                ObjectKey: 'test/transcribe-output/transcript.json'
+              }
+            }
+          ]
+        }
+      }
+
+      job_test(job, 10) do |output|
+        _(output['JobResult']['Job']['Id']).must_equal 'porter-test-transcribe-output'
+        _(output['JobResult']['State']).must_equal 'DONE'
+        _(output['JobResult']['FailedTasks']).must_equal []
+        _(output['JobResult']['TaskResults'].length).must_equal 1
+        _(output['JobResult']['TaskResults'][0]['SubtitleFormats'][0]).must_equal 'vtt'
+      end
+    end
+
     it 'fails with invalid media format' do
       job = {
         Job: {
@@ -28,8 +60,39 @@ describe :porter do
         }
       }
 
-      job_test(job, 5) do |output|
+      job_test(job) do |output|
         _(output['JobResult']['Job']['Id']).must_equal 'porter-test-transcribe-invalid-media-format'
+        _(output['JobResult']['State']).must_equal 'DONE'
+        _(output['JobResult']['TaskResults']).must_equal []
+        _(output['JobResult']['FailedTasks'].length).must_equal 1
+      end
+    end
+
+    it 'fails with invalid subtitle format' do
+      job = {
+        Job: {
+          Id: 'porter-test-transcribe-invalid-subtitle-format',
+          Source: {
+            Mode: 'HTTP',
+            URL: 'https://dovetail.prxu.org/152/245d0fe2-4171-4ebf-bea3-69deff3e9336/short.mp3'
+          },
+          Tasks: [
+            {
+              Type: 'Transcribe',
+              LanguageCode: 'en-US',
+              SubtitleFormats: 'vtt',
+              Destination: {
+                Mode: 'AWS/S3',
+                BucketName: CONFIG.PORTER_TEST_BUCKET_NAME,
+                ObjectKey: 'test/transcribe-invalid-subtitle-format/transcript.json'
+              }
+            }
+          ]
+        }
+      }
+
+      job_test(job) do |output|
+        _(output['JobResult']['Job']['Id']).must_equal 'porter-test-transcribe-invalid-subtitle-format'
         _(output['JobResult']['State']).must_equal 'DONE'
         _(output['JobResult']['TaskResults']).must_equal []
         _(output['JobResult']['FailedTasks'].length).must_equal 1
