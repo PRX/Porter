86d3f0a7d3d967ebf6e398db66d2ec67ae552412
Chris Kalafarski <chris@farski.com>
2022-05-06 15:28:57 -0400

    Refactor copy task
    Makes it easier to add additional destinations down the road. No real changes, just moves existing code around.

---
+++ src/lambdas/copy-task/destination/gcp-storage.js
@@ -0,0 +1,13 @@
+const { Storage } = require('@google-cloud/storage');
+
+module.exports = async function main(event) {
+  // Copies a file in Google Cloud Storage to the S3 artifact bucket.
+  // https://googleapis.dev/nodejs/storage/latest/index.html
+  // https://googleapis.dev/nodejs/storage/latest/Bucket.html#getFiles
+  // https://cloud.google.com/storage/docs/json_api/v1/objects/get
+  // https://github.com/googleapis/nodejs-storage/blob/main/samples/downloadFile.js
+  // const storage = new Storage({
+  //   projectId: event.Job.Source.ProjectId,
+  //   credentials: event.Job.Source.Credentials,
+  // });
+};
--- src/lambdas/copy-task/index.js
+++ src/lambdas/copy-task/index.js
@@ -1,6 +1,5 @@
-const AWS = require('aws-sdk');
-
-const sts = new AWS.STS({ apiVersion: '2011-06-15' });
+const toAwsS3 = require('./destination/aws-s3');
+const toGcpStorage = require('./destination/gcp-storage');
 
 class UnknownCopyTaskModeError extends Error {
   constructor(...params) {
@@ -9,84 +8,18 @@ class UnknownCopyTaskModeError extends Error {
   }
 }
 
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#copyObject-property
-// https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectCOPY.html
-// CopySource expects: "/sourcebucket/path/to/object.extension"
-// CopySource expects "/sourcebucket/path/to/object.extension" to be URI-encoded
-async function awsS3copyObject(event) {
-  console.log(
-    JSON.stringify({
-      msg: 'S3 Copy',
-      source: `${event.Artifact.BucketName}/${event.Artifact.ObjectKey}`,
-      destination: `${event.Task.BucketName}/${event.Task.ObjectKey}`,
-    }),
-  );
-
-  const role = await sts
-    .assumeRole({
-      RoleArn: process.env.S3_DESTINATION_WRITER_ROLE,
-      RoleSessionName: 'porter_copy_task',
-    })
-    .promise();
-
-  const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    accessKeyId: role.Credentials.AccessKeyId,
-    secretAccessKey: role.Credentials.SecretAccessKey,
-    sessionToken: role.Credentials.SessionToken,
-  });
-
-  const params = {
-    CopySource: encodeURI(
-      `/${event.Artifact.BucketName}/${event.Artifact.ObjectKey}`,
-    ).replace(/\+/g, '%2B'),
-    Bucket: event.Task.BucketName,
-    Key: event.Task.ObjectKey,
-  };
-
-  // When the optional `ContentType` property is set to `REPLACE`, if a MIME is
-  // included with the artifact, that should be used as the copy's content type
-  if (
-    Object.prototype.hasOwnProperty.call(event.Task, 'ContentType') &&
-    event.Task.ContentType === 'REPLACE' &&
-    Object.prototype.hasOwnProperty.call(event.Artifact, 'Descriptor') &&
-    Object.prototype.hasOwnProperty.call(event.Artifact.Descriptor, 'MIME')
-  ) {
-    params.MetadataDirective = 'REPLACE';
-    params.ContentType = event.Artifact.Descriptor.MIME;
-  }
-
-  // Assign all members of Parameters to params. Remove the properties required
-  // for the Copy operation, so there is no collision
-  if (Object.prototype.hasOwnProperty.call(event.Task, 'Parameters')) {
-    delete event.Task.Parameters.CopySource;
-    delete event.Task.Parameters.Bucket;
-    delete event.Task.Parameters.Key;
-
-    Object.assign(params, event.Task.Parameters);
-  }
-
-  const start = process.hrtime();
-  await s3.copyObject(params).promise();
-  const end = process.hrtime(start);
-
-  console.log(
-    JSON.stringify({
-      msg: 'Finished S3 Copy',
-      duration: `${end[0]} s ${end[1] / 1000000} ms`,
-    }),
-  );
-}
-
 exports.handler = async (event) => {
   console.log(JSON.stringify({ msg: 'State input', input: event }));
 
-  if (event.Task.Mode === 'AWS/S3') {
-    // TODO Detect if the source file is > 5 GB and do a multipart upload to
-    // create the copy
-    await awsS3copyObject(event);
-  } else {
-    throw new UnknownCopyTaskModeError('Unexpected copy mode');
+  switch (event.Task.Mode) {
+    case 'AWS/S3':
+      await toAwsS3(event);
+      break;
+    case 'GCP/Storage':
+      await toGcpStorage(event);
+      break;
+    default:
+      throw new UnknownCopyTaskModeError('Unexpected copy mode');
   }
 
   const now = new Date();
