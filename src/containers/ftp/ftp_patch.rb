0aa11ea1af9a412738a2c55ef2e6f56cc7b6c8c2
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-07-27 19:54:46 -0400

    Patch Net::FTP to add sending and listening on override port

--- src/containers/ftp/ftp_patch.rb
+++ src/containers/ftp/ftp_patch.rb
@@ -6,13 +6,28 @@ module Net
   # Override the Net::FTP class to add overrides
   #  - based on this closed patch: https://github.com/ruby/ruby/pull/1561
   class FTP
-    # this makes active mode work by setting the public ip of the client
-    attr_accessor :public_host_ip
+    # this makes active mode work by setting the public ip and port of the client
+    attr_accessor :public_host_ip, :public_port_num
 
-    # override with the public_host_ip
+    # override the listening port with public_port_num
+    def makeport_with_override
+      port = [public_port_num.to_i, 0].max
+      host = @bare_sock.local_address.ip_address
+      puts "Net::FTP Patch: makeport_with_override: #{host}:#{port}"
+      Addrinfo.tcp(host, port).listen
+    end
+
+    # rubocop:disable Style/Alias
+    alias_method :makeport_without_override, :makeport
+    alias_method :makeport, :makeport_with_override
+    # rubocop:enable Style/Alias
+
+    # override the host and port with public_host_ip and public_port_num
     def sendport_with_override(host, port)
-      host = public_host_ip if public_host_ip
-      sendport_without_override(host, port)
+      override_host = public_host_ip || host
+      override_port = [public_port_num.to_i, port.to_i].max
+      puts "Net::FTP Patch: sendport_with_override: default: #{host}:#{port}, override: #{override_host}:#{override_port}"
+      sendport_without_override(override_host, override_port)
     end
 
     # rubocop:disable Style/Alias
