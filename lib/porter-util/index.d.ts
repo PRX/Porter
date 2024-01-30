2172b18e20ed9544a6687ffe32c4087792638209
Chris Kalafarski <chris@farski.com>
2024-01-30 10:01:25 -0500

    Refactor artifact writer to shared util

---
+++ lib/porter-util/index.d.ts
@@ -0,0 +1,1 @@
+export function writeArtifact(event: any, context: any): string;
--- src/lambdas/detect-silence/index.js
+++ src/lambdas/detect-silence/index.js
@@ -4,13 +4,7 @@ import { createReadStream, unlinkSync } from 'node:fs';
 import { createInterface } from 'node:readline';
 import { once } from 'node:events';
 import { spawn } from 'node:child_process';
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
-import { writeFile } from 'node:fs/promises';
-
-const s3 = new S3Client({
-  apiVersion: '2006-03-01',
-  followRegionRedirects: true,
-});
+import { writeArtifact } from 'porter-util';
 
 const DEFAULT_MAX_VALUE = 0.001;
 const DEFAULT_MIN_DURATION = 0.2;
@@ -99,27 +93,6 @@ async function getRangesFromMetadataFile(filePath) {
   await once(reader, 'close');
 
   return ranges;
-}
-
-/** Fetches the job's source file artifact from S3 and writes it to the Lambda
- * environment's local temp storage.
- * @returns {Promise<string>} Path to the file that was written
- */
-async function writeArtifact(event, context) {
-  const ext = event.Artifact.Descriptor.Extension;
-  const tmpFilePath = pathJoin(tmpdir(), `${context.awsRequestId}.${ext}`);
-
-  const { Body } = await s3.send(
-    new GetObjectCommand({
-      Bucket: event.Artifact.BucketName,
-      Key: event.Artifact.ObjectKey,
-    }),
-  );
-
-  // @ts-ignore
-  await writeFile(tmpFilePath, Body);
-
-  return tmpFilePath;
 }
 
 export const handler = async (event, context) => {
