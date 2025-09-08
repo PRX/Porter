239aad5e700ea689c6a05afbe6ca14ce955a5d72
Chris Kalafarski <chris@farski.com>
2025-09-08 11:41:22 -0400

    Ignore "any" warning

--- lib/porter-util/index.d.ts
+++ lib/porter-util/index.d.ts
@@ -1,2 +1,3 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: <explanation> */
 export function writeArtifact(event: any, context: any): string;
 export function sendToDestination(event: any, body: any): void;
