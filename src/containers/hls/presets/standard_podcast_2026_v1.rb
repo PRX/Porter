44160345d6e5d8d47085b821ffa90ad49d134e33
Chris Kalafarski <chris@farski.com>
2026-08-13 14:38:34 -0400

    Remove unused preset labels

--- src/containers/hls/presets/standard_podcast_2026_v1.rb
+++ src/containers/hls/presets/standard_podcast_2026_v1.rb
@@ -2,7 +2,7 @@ module Presets
   module StandardPodcast2026
     class V1
       NAME = "Standard Podcast 2026 v1"
-      POSSIBLE_LABELS = %w[AUDIO 240P 360P 480P 720P 1080P 1440P 2160P 2880P 4320P 8640P]
+      POSSIBLE_LABELS = %w[AUDIO 480P 720P 1080P 1440P 2160P]
     end
   end
 end
